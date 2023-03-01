a5458d0be1fdf3858a42fb9e4b12250fa011da78
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-01 13:50:52 -0600

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
+          <p className="text-3xl font-bold underline">
+      Hello LITTLE PIZZA!
+    
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
