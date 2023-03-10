84d1b93647d93fe9b6672d9125fa2bb29d728403
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-09 22:10:44 -0600

    add: initial layout setup

---
+++ pages/components/layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+interface Props {
+    children: JSX.Element | JSX.Element[]
+}
+
+const Layout = ({children}: Props) => {
+  return (
+      <>
+          <Navbar />
+
+          <main>{children}</main>
+          
+          <Footer />
+      </>
+  )
+}
+
+export default Layout
