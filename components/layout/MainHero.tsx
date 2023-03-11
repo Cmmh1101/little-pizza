9025c77a0c962a731a42564e766c8c4011886aa9
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-11 00:05:03 -0600

    hero and nav quick fix

--- components/layout/MainHero.tsx
+++ components/layout/MainHero.tsx
@@ -18,14 +18,14 @@ const MainHero = (props: Props) => {
         <source src="../assets/videos/heroPizza1.mp4" type="video/mp4" />
       </video>
       <div className="container mx-auto text-center z-50">
-        <h1 className="text-6xl text-white font-bold mb-3">
+        <h1 className="text-6xl md:text-8xl text-white font-bold mb-5">
           Little Italy East Memphis
         </h1>
-        <p className="font-medium text-gray-300 text-2xl mb-3">
+        <p className="font-medium text-gray-200 text-2xl mb-5">
           {" "}
           Pizza & Pasta
         </p>
-        <button className="px-4 py-1 text-lg text-white font-semibold rounded-full border border-red-200 bg-red-600 hover:text-white hover:bg-red-500  hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">
+        <button className="px-5 py-2 text-2xl text-white font-semibold rounded-full border border-red-200 bg-red-600 hover:text-white hover:bg-red-500  hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">
           Order Now!
         </button>
       </div>
