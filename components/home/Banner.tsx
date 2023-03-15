d4498f7c19be9b2e7e7ab4f168a5e4bb703f4b0f
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-14 23:58:54 -0500

    add: map

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const Banner = (props: Props) => {
   return (
-    <div className="w-full h-[50vh] lg:px-40 xl:px-60 flex flex-col items-center justify-center bg-green-600">
+    <div className="w-full min-h-[40vh] py-28 px-5 lg:px-40 xl:px-60 flex flex-col items-center justify-center bg-green-600">
           <h3 className="text-3xl uppercase md:text-5xl text-white font-bold text-center">
         Call and Order Your Favorites Now!
         <br />
