c922398aac1c57a81455490ef95abae8101eedea
Carla Montano <cm.emprendiendo@gmail.com>
2024-06-18 23:36:15 -0500

    change images

--- data/menuItemsData.ts
+++ data/menuItemsData.ts
@@ -1,31 +1,31 @@
 export const menuItemsData = [
     {
-        image: "/assets/images/menuItems/appetizers.png",
+        image: "/assets/images/menuItems/appetizers-1.png",
         name: "Appetizers",
         url: "https://togoorder.com/web/5989#/o/2/f/1/m/18025//s/306703/items"
     },
     {
-        image: "/assets/images/menuItems/salad.png",
+        image: "/assets/images/menuItems/salad-1.png",
         name: "Salads & soups",
         url: "https://togoorder.com/web/5989#/o/2/f/1/m/18025//s/306704/items"
     },
     {
-        image: "/assets/images/menuItems/panini.png",
+        image: "/assets/images/menuItems/panini-1.png",
         name: "Panini",
         url: "https://togoorder.com/web/5989#/o/2/f/1/m/18025//s/306708/items"
     },
     {
-        image: "/assets/images/menuItems/pizza.png",
+        image: "/assets/images/menuItems/pizza-1.png",
         name: "Pizza",
         url: "https://togoorder.com/web/5989#/o/2/f/1/m/18025//s/306712/items"
     },
     {
-        image: "/assets/images/menuItems/pasta.png",
+        image: "/assets/images/menuItems/pasta-1.png",
         name: "Pasta",
         url: "https://togoorder.com/web/5989#/o/2/f/1/m/18025//s/306706/items"
     },
     {
-        image: "/assets/images/menuItems/dessert.png",
+        image: "/assets/images/menuItems/dessert-1.png",
         name: "Dessert",
         url: "https://togoorder.com/web/5989#/o/2/f/1/m/18025//s/306709/items"
     },
