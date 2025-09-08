0229550a5c0ca26375afaad1dfe3adb33e070972
Carla Montano <cm.emprendiendo@gmail.com>
2025-09-08 16:11:59 -0400

    netlify form

--- components/contact/Form.tsx
+++ components/contact/Form.tsx
@@ -4,19 +4,35 @@ type Props = {}
 
 const Form = (props: Props) => {
     return (
-        <form name="contact" method="POST" className='text-black md:pl-0 md:pr-3' >
-          <input type="hidden" name="form-name" value="contact" />
+        <form name="contact" method="POST" className='text-black md:pl-0 md:pr-3' data-netlify="true" netlify-honeypot="bot-field" action="/success"
+      acceptCharset="UTF-8">
+        {/* Required for Netlify to map the submission to this form */}
+        <input type="hidden" name="form-name" value="contact" />
+        {/* Honeypot (hidden anti-spam field) */}
+        <p hidden>
+          <label>
+            Don't fill this out: <input name="bot-field" />
+          </label>
+        </p>
           <p>
             <label htmlFor="yourname" className='text-white'>
               Your Name:
             </label> <br />
-            <input className='w-full mx-auto px-3' type="text" name="name" id="yourname" placeholder='Full name' />
+          <input
+            className='w-full mx-auto px-3' type="text"
+            name="name"
+            id="yourname"
+            placeholder='Full name' />
           </p>
           <p>
             <label htmlFor="youremail" className='text-white'>
               Your Email:
             </label> <br />
-            <input type="email" name="email" id="youremail" className='w-full mx-auto px-3' placeholder='Email' />
+          <input
+            type="email"
+            name="email"
+            id="youremail"
+            className='w-full mx-auto px-3' placeholder='Email' />
           </p>
           <p>
             <label htmlFor="yourmessage" className='text-white'>
