4dd90d00f02768fe3f01c07db8da714ca9e355ab
Carla Montano <cm.emprendiendo@gmail.com>
2025-09-08 14:02:23 -0400

    add netlify file

--- components/catering/ReactZoom.tsx
+++ components/catering/ReactZoom.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import classes from "./reactZoom.module.css";
 import { FiZoomIn, FiZoomOut, FiRefreshCcw } from "react-icons/fi";
 
